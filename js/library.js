7a09281ad13fa624018b3964d44606c3eb40d3b6
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-09-02 19:08:37 +0600

    error msg handled and img not found added

--- js/library.js
+++ js/library.js
@@ -9,7 +9,11 @@ const searchBook = () => {
     // clear search field data 
     searchField.value = '';
     if (searchText == '') {
-        document.getElementById('noBooks-found').style.display = 'block';
+        const foundNoBook = document.getElementById('noBooks-found');
+        foundNoBook.style.display = 'block';
+        foundNoBook.innerHTML = `
+        <h3 class="text-danger">Empty search box</h3>
+        `;
         // clear field 
         const totalResultDiv = document.getElementById('total-results');
         const searchResultDiv = document.getElementById('search-result');
@@ -28,22 +32,37 @@ const searchBook = () => {
             .then(data => displaySearchResult(data.numFound, data.docs));
     }
 }
+// show not found in books description 
+const notFoundResult = data => {
+    if (data === undefined) {
+        data = 'Not found';
+        return data;
+    }
+    else {
+        return data;
+    }
+}
 // display books after searching
 const displaySearchResult = (totalResults, books) => {
-    console.log(totalResults);
-    console.log(books);
+    // console.log(totalResults);
+    // console.log(books.length);
+    // console.log(books);
 
     // total results show 
     const totalResultDiv = document.getElementById('total-results');
     totalResultDiv.textContent = '';
     const h3 = document.createElement('h3');
     h3.innerHTML = `
-    <h3>Total Results of books: ${totalResults}</h3>
+    <h3 class="text-center text-danger mb-4">Showing results ${books.length} of ${totalResults}</h3>
     `;
     totalResultDiv.appendChild(h3);
     // no books found show 
     if (totalResults === 0) {
-        document.getElementById('noBooks-found').style.display = 'block';
+        const noBookFound = document.getElementById('noBooks-found');
+        noBookFound.style.display = 'block';
+        noBookFound.innerHTML = `
+        <h3 class="text-danger">No books found</h3>
+        `;
     }
     else {
         document.getElementById('noBooks-found').style.display = 'none';
@@ -63,24 +82,26 @@ const displaySearchResult = (totalResults, books) => {
         div.classList.add('book');
         div.classList.add('col');
 
-        if (typeof (book.author_name) === undefined) {
-            const authorName = book.author_alternative_name;
+        const authorName = notFoundResult(book.author_name);
+        const publisher = notFoundResult(book.publisher);
+        const firstPublishYear = notFoundResult(book.first_publish_year);
+        // book image show 
+        let coverImageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        if (book.cover_i === undefined) {
+            coverImageUrl = `images/no image available.png`;
         }
         else {
-            const authorName = book.author_name;
+            coverImageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         }
-        coverImageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-
         {/* <p class="card-text">${meal.strInstructions.slice(0, 200)}</p> */ }
         div.innerHTML = `
-        <div class="card h-100">
-            
+        <div class="card h-100 border border-secondary border-2 shadow-sm">        
             <div class="card-body">
-                <img height="350px"  src="${coverImageUrl}" class="card-img-top" alt="..."></img>
-                <h5 class="card-title mt-5">Book Name: ${book.title}</p></h5>
-                <h5>Author Name: ${book.author_name}</p></h5>
-                <h5>Publisher: ${book.publisher}</h5>
-                <h5>First Publish Year: ${book.first_publish_year}</h5>
+                <img height="250px"  src="${coverImageUrl}" class="card-img-top" alt="..."></img>
+                <h6 class="text-secondary fs-5 mt-3">${book.title}</h6>
+                <p class="text-primary fw-bolder"><span class="text-primary fw-bold">by </span>${authorName}</p>
+                <p><span class="fw-bolder">Publisher: </span>${publisher}</p>
+                <p><span class="fw-bolder">First Publish Year: </span>${firstPublishYear}</p>
             </div>
         </div>
             `;
